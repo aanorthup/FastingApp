3133f71748ec680888e68833df936c45658951fe
Ashley Northup <aan0013@auburn.edu>
2023-11-11 17:28:19 -0500

    updated file

--- FastingApp/FastingModel.swift
+++ FastingApp/FastingModel.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Fasting: Identifiable {
+    var id = UUID()
+    var startDate: Date
+    var endDate: Date
+    var weight: Double
+    var measurements: Measurements
+}
+
+struct Measurements {
+    var waist: Double
+    var hips: Double
+    var chest: Double
+    var leftArm: Double
+    var rightArm: Double
+    var leftThigh: Double
+    var rightThigh: Double
+}
