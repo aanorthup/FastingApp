95c3f61edf8d944f02aa4b29f617af72b681ba93
Ashley Northup <aan0013@auburn.edu>
2023-11-11 16:54:42 -0500

    started fasting app

---
+++ FastingApp/FastingModel.swift
@@ -0,0 +1,8 @@
+//
+//  FastingModel.swift
+//  FastingApp
+//
+//  Created by admin on 11/11/23.
+//
+
+import Foundation
---
+++ FastingApp/FastingViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  FastingViewModel.swift
+//  FastingApp
+//
+//  Created by admin on 11/11/23.
+//
+
+import Foundation
