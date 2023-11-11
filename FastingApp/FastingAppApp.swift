1bd3cff6fd718170b8eb3941ea66c31759340846
Ashley Northup <aan0013@auburn.edu>
2023-11-11 16:48:57 -0500

    Initial Commit

---
+++ FastingApp/FastingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingAppApp.swift
+//  FastingApp
+//
+//  Created by admin on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct FastingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
